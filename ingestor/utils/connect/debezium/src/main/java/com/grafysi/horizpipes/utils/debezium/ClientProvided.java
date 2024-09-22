f402a9b35e3e138a13f7cf15313e91eccd1a55d1
htringuyen <tringuyenh75@gmail.com>
2024-09-22 21:22:52 +0700

    Debezium patch to change default serialization behaviour

---
+++ ingestor/utils/connect/debezium/src/main/java/com/grafysi/horizpipes/utils/debezium/ClientProvided.java
@@ -0,0 +1,6 @@
+package com.grafysi.horizpipes.utils.debezium;
+
+import io.debezium.engine.format.SerializationFormat;
+
+public class ClientProvided implements SerializationFormat<Object> {
+}
---
+++ ingestor/utils/connect/debezium/src/main/java/com/grafysi/horizpipes/utils/debezium/HzpConvertingAsyncEngineBuilderFactory.java
@@ -0,0 +1,100 @@
+package com.grafysi.horizpipes.utils.debezium;
+
+import io.debezium.embedded.ConverterBuilder;
+import io.debezium.embedded.async.AsyncEmbeddedEngine;
+import io.debezium.engine.ChangeEvent;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.engine.RecordChangeEvent;
+import io.debezium.engine.format.ChangeEventFormat;
+import io.debezium.engine.format.KeyValueChangeEventFormat;
+import io.debezium.engine.format.KeyValueHeaderChangeEventFormat;
+import io.debezium.engine.format.SerializationFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HzpConvertingAsyncEngineBuilderFactory implements DebeziumEngine.BuilderFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HzpConvertingAsyncEngineBuilderFactory.class);
+
+    @Override
+    public <T, V extends SerializationFormat<T>> DebeziumEngine.Builder<RecordChangeEvent<T>> builder(ChangeEventFormat<V> format) {
+        var builder = (AsyncEmbeddedEngine.AsyncEngineBuilder<RecordChangeEvent<T>>) newAsyncEngineBuilder(ChangeEventFormat.class, format);
+        setConverterBuilder(builder, new HzpConverterBuilder<>());
+        return builder;
+    }
+
+    @Override
+    public <S, T, K extends SerializationFormat<S>, V extends SerializationFormat<T>> DebeziumEngine.Builder<ChangeEvent<S, T>> builder(
+            KeyValueChangeEventFormat<K, V> format) {
+        var builder = (AsyncEmbeddedEngine.AsyncEngineBuilder<ChangeEvent<S, T>>) newAsyncEngineBuilder(KeyValueChangeEventFormat.class, format);
+        setConverterBuilder(builder, new HzpConverterBuilder<>());
+        return builder;
+    }
+
+    public <S, T, U, K extends SerializationFormat<S>, V extends SerializationFormat<T>, H extends SerializationFormat<U>> DebeziumEngine.Builder<ChangeEvent<S, T>> builder(
+            KeyValueHeaderChangeEventFormat<K, V, H> format) {
+        var builder = (AsyncEmbeddedEngine.AsyncEngineBuilder<ChangeEvent<S, T>>) newAsyncEngineBuilder(KeyValueHeaderChangeEventFormat.class, format);
+        setConverterBuilder(builder, new HzpConverterBuilder<>());
+        return builder;
+    }
+
+    private AsyncEmbeddedEngine.AsyncEngineBuilder<?> newAsyncEngineBuilder(Class<?> formatClass, Object format) {
+        try {
+            var clazz = Class.forName(AsyncEmbeddedEngine.AsyncEngineBuilder.class.getName());
+            var constructor = clazz.getDeclaredConstructor(formatClass);
+            constructor.setAccessible(true);
+            return (AsyncEmbeddedEngine.AsyncEngineBuilder<?>) constructor.newInstance(format);
+        } catch (Exception e) {
+            LOGGER.error("Instantiate {} failed.", AsyncEmbeddedEngine.AsyncEngineBuilder.class.getName());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setConverterBuilder(AsyncEmbeddedEngine.AsyncEngineBuilder<?> engineBuilder, ConverterBuilder<?> converterBuilder) {
+        try {
+            var field = AsyncEmbeddedEngine.AsyncEngineBuilder.class.getField("converterBuilder");
+            field.setAccessible(true);
+            field.set(engineBuilder, converterBuilder);
+        } catch (Exception e) {
+            LOGGER.error("Set converter builder failed.");
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
